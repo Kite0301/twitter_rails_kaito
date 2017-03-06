7fe442f22760281753b0848fd34c99c7a7921d0c
Kite0301 <kaito.titech@gmail.com>
2017-03-06 16:21:36 +0900

    Add Lesson9

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170228073253) do
+ActiveRecord::Schema.define(version: 20170306065335) do
 
   create_table "posts", force: :cascade do |t|
-    t.text "content"
+    t.text    "content"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
